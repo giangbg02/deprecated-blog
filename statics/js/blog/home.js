14502954a28c75d73e692ed663e593c0d254123f
Tmiracle <1720570275@qq.com>
2020-01-15 05:12:07 +0800

    v1.0.17 博客布局更新

--- statics/js/blog/home.js
+++ statics/js/blog/home.js
@@ -2,4 +2,19 @@ document.querySelector("#show-aside-btn").addEventListener("click", function () 
     document.querySelector(".container>aside").classList.toggle("show");
 });
 
-
+$(function(){$(function() {
+    var Fwidth = document.querySelector("main").offsetWidth;
+    var offset = $('#aside').offset();
+    $("main").scroll(function () {
+        //检查对象的顶部是否在游览器可见的范围内
+        var scrollTop = $("main").scrollTop();
+        if (offset.top < scrollTop + 65){
+            $('#aside').addClass('fixed');
+            $('#aside').css("width", (Fwidth-30)*0.25);
+        }
+        else{
+            $('#aside').removeClass('fixed');
+        }
+    });
+});
+});
