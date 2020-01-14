242d13c2feb61d890a7e6989eff12874e022accc
Tmiracle <1720570275@qq.com>
2020-01-15 05:37:28 +0800

    v1.0.20

--- statics/js/blog/home.js
+++ statics/js/blog/home.js
@@ -6,14 +6,13 @@ document.querySelector("#show-aside-btn").addEventListener("click", function () 
 // 目录粘性定位
 $(function () {
     $(function () {
-        var main_width = document.querySelector("main").offsetWidth;
-        var art_width = document.querySelector(".article-main").offsetWidth;
+        var getWidth = document.querySelector(".article-aside").offsetWidth;
         var offset = $('#aside').offset();
         $("main").scroll(function () {
             //检查对象的顶部是否在游览器可见的范围内
             var scrollTop = $("main").scrollTop();
             if (offset.top < scrollTop + 65) {
-                $('#aside').addClass('fixed').css("width", main_width - art_width);
+                $('#aside').addClass('fixed').css("width", getWidth - 1);
             } else {
                 $('#aside').removeClass('fixed');
             }
