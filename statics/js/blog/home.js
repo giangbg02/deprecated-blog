ff55c9e55da75145e105652c170ad10bba964862
Tmiracle <1720570275@qq.com>
2020-01-15 05:23:22 +0800

    v1.0.18 侧边栏定位js更新

--- statics/js/blog/home.js
+++ statics/js/blog/home.js
@@ -2,19 +2,19 @@ document.querySelector("#show-aside-btn").addEventListener("click", function () 
     document.querySelector(".container>aside").classList.toggle("show");
 });
 
-$(function(){$(function() {
-    var Fwidth = document.querySelector("main").offsetWidth;
-    var offset = $('#aside').offset();
-    $("main").scroll(function () {
-        //检查对象的顶部是否在游览器可见的范围内
-        var scrollTop = $("main").scrollTop();
-        if (offset.top < scrollTop + 65){
-            $('#aside').addClass('fixed');
-            $('#aside').css("width", (Fwidth-30)*0.25);
-        }
-        else{
-            $('#aside').removeClass('fixed');
-        }
+$(function () {
+    $(function () {
+        var main_width = document.querySelector("main").offsetWidth;
+        var art_width = document.querySelector(".article-main").offsetWidth;
+        var offset = $('#aside').offset();
+        $("main").scroll(function () {
+            //检查对象的顶部是否在游览器可见的范围内
+            var scrollTop = $("main").scrollTop();
+            if (offset.top < scrollTop + 65) {
+                $('#aside').addClass('fixed').css("width", main_width - art_width - 10);
+            } else {
+                $('#aside').removeClass('fixed');
+            }
+        });
     });
-});
 });
