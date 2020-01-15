05355ae0a395baf983dda9eda52775d26d90e5b9
Tmiracle <1720570275@qq.com>
2020-01-16 03:13:19 +0800

    v1.0.31 移除原导航

--- statics/js/navigation.js
+++
@@ -1,6 +0,0 @@
-$(".desc span").on('mouseenter', function() {
-    var that = this;
-    layer.tips(that.innerText, that, {tips: [3, '#78BA32'], time: 10000});
-}).on('mouseleave', function() {
-    layer.closeAll();
-});
