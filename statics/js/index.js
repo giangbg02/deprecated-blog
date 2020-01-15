b4a0f6a9f6adb78a51d0280f8d17316d0c1eb363
Tmiracle <1720570275@qq.com>
2020-01-16 02:57:29 +0800

    v1.0.28

--- statics/js/index.js
+++ statics/js/index.js
@@ -4,18 +4,15 @@ document.querySelector("#show-aside-btn").addEventListener("click", function () 
 });
 
 // 右侧文章目录粘性定位
-$(".main-body").onresize = function(){
-    $(function () {
-        var getWidth = document.querySelector(".article-aside").offsetWidth;
-        var offset = $('#aside').offset();
-        $("main").scroll(function () {
-            //检查对象的顶部是否在游览器可见的范围内
-            var scrollTop = $("main").scrollTop();
-            if (offset.top < scrollTop + 65) {
-                $('#aside').addClass('fixed').css("width", getWidth - 15);
-            } else {
-                $('#aside').removeClass('fixed');
-            }
-        });
+$(function () {
+    var offset = $('#aside').offset();
+    $("main").scroll(function () {
+        //检查对象的顶部是否在游览器可见的范围内
+        var scrollTop = $("main").scrollTop();
+        if (offset.top < scrollTop + 65) {
+            $('#aside').addClass('fixed');
+        } else {
+            $('#aside').removeClass('fixed');
+        }
     });
-};
+});
