2aa1875ee125080930b2ea5964d3790fe68eaf6a
Tmiracle <1720570275@qq.com>
2020-01-16 20:00:52 +0800

    v1.0.35 添加网站访问次数

--- statics/js/count.js
+++ statics/js/count.js
@@ -1,4 +1,4 @@
 let targetUrl = window.location.href;
 $.ajax('https://hitcounter.pythonanywhere.com/count',{
     data:{url: targetUrl},
-}).then(count => $(".count").html("<span>本站总访问量：" + (parseInt(count) + 996) + "次</span>"));
+}).then(count => $(".count").html("<span>本站总访问量：" + count + "次</span>"));
