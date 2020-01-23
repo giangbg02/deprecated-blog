ab1701765ac5b928d64789bf57ceb869dbe130b5
Tmiracle <1720570275@qq.com>
2020-01-24 00:36:55 +0800

    v1.0.59 文章样式更新，gitalk优化

--- statics/js/index.js
+++ statics/js/index.js
@@ -1,6 +1,7 @@
 $(function () {
     GenerateContentList();
 
+    // pjax异步加载页面
     $(document).pjax('a', '.main-body', {
         fragment: '.main-body',
         timeout: 5000,
@@ -35,11 +36,13 @@ function GenerateContentList() {
         }
     });
 
+    // 当前网页访问量统计
     let targetUrl = window.location.href;
     $.ajax('https://hitcounter.pythonanywhere.com/count', {
         data: {url: targetUrl},
     }).then(count => $(".num").html(count));
 
+    // 未完成文章点击提示信息
     $('.article-tip').click(function () {
         top.layer.msg(
             '<svg style="width: 36px;height: 36px" class="icon" aria-hidden="true"><use xlink:href="#icon-biji"></use></svg>文章正在努力编写中...',
@@ -50,6 +53,7 @@ function GenerateContentList() {
             })
     });
 
+    // 加载gitalk评论系统
     if ($('#gitalk-container').length > 0) {
         var gitalk = new Gitalk({
             clientID: '7ff38d8c3509efdcee12',   // GitHub Application Client ID
@@ -60,8 +64,6 @@ function GenerateContentList() {
             id: md5(location.pathname),         // 用于标记评论是哪个页面的，确保唯一，并且长度小于50
         });
         gitalk.render('gitalk-container');      // 渲染Gitalk评论组件
-    } else {
-        console.log('没有gitalk')
     }
 
 }
