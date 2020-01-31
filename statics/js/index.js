1c59eafd43d81e8247e7417875b4271c49e4346c
Tmiracle <1720570275@qq.com>
2020-01-31 22:56:54 +0800

    v1.0.62 解决pjax加载页面后js不生效问题（✔）

--- statics/js/index.js
+++ statics/js/index.js
@@ -52,28 +52,4 @@ function GenerateContentList() {
                 btn: ['明白了']
             })
     });
-
-    // 匹配加载的页面中是否含有js
-    var regDetectJs = /<script(.|\n)*?>(.|\n|\r\n)*?<\/script>/ig;
-    //ajaxLoadedData为ajax获取到的数据
-    var jsContained = ajaxLoadedData.match(regDetectJs);
-    // 如果包含js，则一段一段的取出js再加载执行
-    if(jsContained) {
-        // 分段取出js正则
-        var regGetJS = /<script(.|\n)*?>((.|\n|\r\n)*)?<\/script>/im;
-        // 按顺序分段执行js
-        var jsNums = jsContained.length;
-        for (var i=0; i<jsNums; i++) {
-            var jsSection = jsContained[i].match(regGetJS);
-            if(jsSection[2]) {
-                if(window.execScript) {
-                    // 给IE的特殊待遇
-                    window.execScript(jsSection[2]);
-                } else {
-                    // 给其他大部分浏览器用的
-                    window.eval(jsSection[2]);
-                }
-            }
-        }
-    }
 }
